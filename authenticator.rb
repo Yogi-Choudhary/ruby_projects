8d809980662c1b11557cc6ab8c3dcb66de490261
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2021-04-18 14:47:33 +0530

    initial

---
+++ authenticator.rb
@@ -0,0 +1,44 @@
+users = [
+          {username: "yogi", password: "yogi"},
+          {username: "deep", password: "deep"},
+          {username: "yogesh", password: "yogi"}
+
+        ]
+
+def auth_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && user_record[:password] == password
+      return user_record
+    end
+  end
+  "Credential were not correct"
+
+end        
+
+puts "Welcome to the authenticator"
+25.times { print "-" }
+puts
+puts "This program will take input from the user and compare password"
+puts "If password is correct, you will get back the user object"
+
+attempts = 1
+
+while attempts < 4
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp
+
+  authentication = auth_user(username,password,users)
+  puts authentication
+
+  puts "Press n to quit or any other ket to continue: "
+  input = gets.chomp.downcase
+  break if input == "n"
+  attempts += 1
+end
+
+puts "You have exceededthe number of attempts" if attempts == 4
+
+
+
